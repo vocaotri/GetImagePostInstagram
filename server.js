31efc83213cda647c0e373a116db84eca0d4f279
Tri Vo <vocaotri445@gmail.com>
2021-05-06 09:33:14 +0700

    update allowedOrigins

--- server.js
+++ server.js
@@ -10,6 +10,41 @@ const { exec } = require('child_process');
 fs = require('fs');
 var port = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+    const allowedOrigins = [
+        "http://127.0.0.1:3000",
+        "http://localhost:3000",
+        "http://localhost:8000",
+        "http://127.0.0.1:8080",
+        "http://localhost:9000",
+        "https://pocchi.live/"
+    ];
+    const origin = req.headers.origin;
+    // Website you wish to allow to connect
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept,x-socket-id,x-csrf-token"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/get-image/:userName?', async (req, res) => {
     let username = req.params.userName ? req.params.userName : "pocchi.live";
     fs.writeFileSync(`getPost.${username}.js`, `require("tools-for-instagram");
